80212c07cc3cef7685904049eb57281834bfbb96
ひびき <20015683+hibikidesu@users.noreply.github.com>
2022-05-12 00:06:50 +0900

    name change

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gazelle-api-py
 Gazelle API for Python
+
+# Installation
+```
+python3 -m pip install -U gazelle-client
+```
--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ requirements = [
 
 
 setup(
-    name="gazelle_api",
+    name="gazelle_client",
     version="1.0.0",
     description="Python API for Gazelle",
     url="https://github.com/hibikidesu/gazelle_api_py",
     author="Hibiki",
     license_files=("LICENSE",),
-    py_modules=["gazelle_api"],
+    py_modules=["gazelle_client"],
     python_requires=">=3.7",
     install_requires=requirements,
     classifiers=[
